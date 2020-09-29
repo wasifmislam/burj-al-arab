3d1db5fa1e42755e700b0c9201f4ded0b816d7f5
wasifmislam <67533737+wasifmislam@users.noreply.github.com>
2020-09-29 13:21:28 +0600

    Login authentication done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB28XyM8WMiRgie7NWvbr0bhR6Z_WVjBr0",
+    authDomain: "was-form.firebaseapp.com",
+    databaseURL: "https://was-form.firebaseio.com",
+    projectId: "was-form",
+    storageBucket: "was-form.appspot.com",
+    messagingSenderId: "191700130030",
+    appId: "1:191700130030:web:8d6f2215ef18cafe927bff"
+  };
+  export default firebaseConfig;
